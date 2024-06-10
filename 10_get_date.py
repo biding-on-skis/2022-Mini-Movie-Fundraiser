8dc446c0762a80c0c50ccd52a489ce5420239168
biding-on-skis <165841246+biding-on-skis@users.noreply.github.com>
2024-06-10 14:57:53 +1200

    Add files via upload

--- 10_get_date.py
+++ 10_get_date.py
@@ -6,4 +6,14 @@ from datetime import date
 # get today's date
 today = date.today()
 
-# Get day, month
+# Get day, month and year as individual strings
+day = today.strftime("%d")
+month = today.strftime("%m")
+year = today.strftime("%y")
+
+heading = "---Mini Movie Fundraiser Ticket Data ({}/{}/{}) ---\n".format(day, month, year)
+filename = "MMF_{}_{}_{}".format(year, month, year)
+
+#heading
+print(heading)
+print("The filename will be {}.txt.".format(filename))
